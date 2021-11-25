3d82ba6c0d57a1b3637b0304a6c46eb741f32312
JHades <jhades.dev@gmail.com>
2021-11-25 14:32:54 +0100

    responsive design preparation

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,8 +1,9 @@
 import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {Course} from "../model/course";
-import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
-import {openEditCourseDialog} from '../course-dialog/course-dialog.component';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import { MatDialog } from "@angular/material/dialog";
 import {filter} from 'rxjs/operators';
+import {openEditCourseDialog} from '../course-dialog/course-dialog.component';
 
 @Component({
     selector: 'courses-card-list',
@@ -14,10 +15,50 @@ export class CoursesCardListComponent implements OnInit {
     @Input()
     courses: Course[];
 
-    constructor(private dialog: MatDialog) {
+    cols = 3;
+    rowHeight = "500px";
+
+    handsetPortrait:boolean = false;
+
+
+    constructor(
+      private dialog: MatDialog,
+      private responsive: BreakpointObserver) {
     }
 
     ngOnInit() {
+
+      this.responsive.observe("(max-width: 959px)")
+        .subscribe(result => console.log(result));
+
+      this.responsive.observe([
+          Breakpoints.TabletPortrait,
+          Breakpoints.TabletLandscape,
+          Breakpoints.HandsetPortrait,
+          Breakpoints.HandsetLandscape
+        ])
+        .subscribe(result => {
+
+          this.handsetPortrait = false;
+
+          const breakpoints = result.breakpoints;
+
+          if (breakpoints[Breakpoints.TabletPortrait]) {
+            this.cols = 1;
+          }
+          else if (breakpoints[Breakpoints.HandsetPortrait]) {
+            this.handsetPortrait = true;
+            this.cols = 1;
+            this.rowHeight = "430px";
+          }
+          else if (breakpoints[Breakpoints.HandsetLandscape]) {
+            this.cols = 1;
+          }
+          else if (breakpoints[Breakpoints.TabletLandscape] ) {
+            this.cols = 2;
+          }
+
+        });
 
     }
 
